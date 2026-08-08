bb1297e0c30b23dc6b6a478897728cf3161e7faa
vichoclo98 <vichoclo98@gmail.com>
2026-08-08 09:30:25 -0400

    noc
    noc

--- scenes/hola.py
+++ scenes/hola.py
@@ -16,38 +16,18 @@ class Lanzamiento(Scene):
         aceleración =always_redraw(
                          lambda: Arrow(start=[2*t.get_value()-4,3*t.get_value()-3,0], end=[2*t.get_value()-4,3*t.get_value()-1*t.get_value()**2-3,0], color=RED)
                 )
+        distancia =always_redraw(
+                                 lambda: Arrow(start=[-4,-3,0], end=[2*t.get_value()-4,3*t.get_value()-1*t.get_value()**2-3,0], color=WHITE)
+                        )
         trazo=TracedPath(circle.get_center)
         velotext=MathTex(r"\vec{v}\cdot t").add_updater(lambda m: m.next_to(puntofalso, LEFT))
         acetext=MathTex(r"\frac{\vec{g}\cdot t^2}{2}").add_updater(lambda x: x.next_to(aceleración, RIGHT))
         self.add(trazo,puntofalso)
         self.play(FadeIn(circle))
-        self.add(velocidad,aceleración,velotext,acetext)
+        self.add(velocidad,aceleración,distancia,velotext,acetext)
         self.play(
 	        t.animate.set_value(3), #define a que valor se cambia t
 	        run_time=4, #cuantos segundos dura la animación, vale 1 si se omite la línea
 	        rate_func=linear #velocidad de cambio constante, puede hacerse un ida y vuelta, animación suave (por defecto), etc.
         )
-        self.wait()
-
-class Prueba(Scene): #hecho por chatGPT, no lo entiendo bien aun
-    def construct(self):
-        v1 = 1
-        v2 = 0.5
-
-        t = ValueTracker(0)
-
-        vector = always_redraw(
-            lambda: Arrow(
-                start=ORIGIN,
-                end=axes.c2p(v1 * t.get_value(),
-                             v2 * t.get_value()**2),
-                buff=0,
-                color=YELLOW
-            )
-        )
-
-        self.add(vector)
-
-        self.play(t.animate.set_value(4), run_time=4, rate_func=linear)
-
         self.wait()
